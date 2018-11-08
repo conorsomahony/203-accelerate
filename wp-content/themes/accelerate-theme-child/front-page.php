69e47640de03b7882e8150cc0e5b22ef5f043e3b
Conor O Mahony <conor.s.omahony@gmail.com>
2018-11-08 11:25:59 +0000

    custom posts type for case_studies, custom query for case_studies in front-page.php, case studies archive page, single case stuies page, and media uploads for case studies

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -12,29 +12,55 @@
  * @since Accelerate Marketing 2.0
  */
 
-get_header(); ?>
+get_header();?>
 
-	<div id="primary" class="home-page hero-content">
-		<div class="main-content" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php the_content(); ?>
-				<a class="button" href="<?php echo site_url('/blog/') ?>">View Our Work</a>
-			<?php endwhile; // end of the loop. ?>
-		</div><!-- .main-content -->
-	</div><!-- #primary -->
+<div id="primary" class="home-page hero-content">
+	<div class="main-content" role="main">
+		<?php while (have_posts()): the_post();?>
+		<?php the_content();?>
+		<a class="button" href="<?php echo site_url('/blog/') ?>">View Our Work</a>
+		<?php endwhile; // end of the loop. ?>
+	</div><!-- .main-content -->
+</div><!-- #primary -->
 
-	<section class="recent-posts">
-		<div class="site-content">
-			<div class="blog-post">
-				<h4>From the Blog</h4>
-				<?php query_posts('posts_per_page=1'); ?>
-				<?php while ( have_posts() ) : the_post(); ?>
-				<h3><?php the_title(); ?></h3>
-				<?php the_excerpt(); ?> 
-				<?php endwhile; ?> 
-				<?php wp_reset_query(); ?>
-			</div>
+<section class="featured-work">
+	<div class="site-content">
+		<h4>Featured Work</h4>
+		<ul class="homepage-featured-work">
+			<?php query_posts('posts_per_page=3&post_type=case_studies');?>
+			<?php while (have_posts()): the_post();
+				$image = get_field("image_1");
+				$size = "medium";
+				?>
+			<li class="featured-work-individual">
+				<figure>
+					<?php echo wp_get_attachment_image($image, $size); ?>
+				</figure>
+				<h6>
+					<a href="<?php the_permalink();?>">
+						<?php the_title();?></a>
+				</h6>
+			</li>
+			<?php endwhile;?>
+			<?php wp_reset_query();?>
+		</ul>
+	</div>
+</section>
+
+<section class="recent-posts">
+	<div class="site-content">
+		<div class="blog-post">
+			<h4>From the Blog</h4>
+			<?php query_posts('posts_per_page=1');?>
+			<?php while (have_posts()): the_post();?>
+			<h3>
+				<?php the_title();?>
+			</h3>
+			<?php the_excerpt();?>
+			<?php endwhile;?>
+			<?php wp_reset_query();?>
 		</div>
-	</section>
+	</div>
+</section>
 
-<?php get_footer(); ?>
+<?php get_footer();?>
