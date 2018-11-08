de59d516a792d97668c69a1cf183af88f6f49624
Conor O Mahony <conor.s.omahony@gmail.com>
2018-11-08 14:30:55 +0000

    fix homepage button, some styling for css classes inserted using the menu plugin in the admin

--- wp-content/themes/accelerate-theme-child/functions.php
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -13,7 +13,10 @@
 // Enqueue scripts and styles
 function accelerate_child_scripts(){
 	wp_enqueue_style( 'accelerate-style', get_template_directory_uri() . '/style.css' );
-	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'accelerate-style' ));
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'accelerate-style' ));
+    
+    wp_register_style( 'fontawesome', 'http:////maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css' );
+    wp_enqueue_style( 'fontawesome');
 }
 add_action( 'wp_enqueue_scripts', 'accelerate_child_scripts' );
 
